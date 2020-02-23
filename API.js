3662b44a16f80507ccd7a0a52840bc535836465f
Bastien <bastienxs@gmail.com>
2020-02-23 11:40:43 +0100

    Auto stash before checking out "HEAD"

--- API.js
+++ API.js
@@ -1,13 +1,13 @@
 const baseUrl = 'https://afternoon-waters-49321.herokuapp.com';
 
-export default function GetPlaylists(){
+export function GetPlaylists() {
     let url = baseUrl + '/v1/browse/featured-playlists';
     return fetch(url)
         .then((response) => response.json())
         .catch((error) => console.error(error))
 }
 
-export function GetPlaylistDetail(idPlayslit){
+export function GetPlaylistDetail(idPlayslit) {
     let url = baseUrl + '/v1/playlists/' + idPlayslit;
     return fetch(url)
         .then((response) => response.json())
