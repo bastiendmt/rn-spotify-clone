0f58579389f981cd956d20660f1b4f4bd4ff68c4
Bastien <bastienxs@gmail.com>
2020-02-23 11:29:05 +0100

    api getPlaylistDetail

--- API.js
+++ API.js
@@ -6,3 +6,10 @@ export default function GetPlaylists(){
         .then((response) => response.json())
         .catch((error) => console.error(error))
 }
+
+export function GetPlaylistDetail(idPlayslit){
+    let url = baseUrl + '/v1/playlists/' + idPlayslit;
+    return fetch(url)
+        .then((response) => response.json())
+        .catch((error) => console.error(error))
+}
